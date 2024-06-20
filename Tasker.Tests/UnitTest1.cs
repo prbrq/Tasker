d20c2790aa50e9a831357e8e243aae3f95e15594
Arkady <prbrq@ya.ru>
2024-06-20 18:01:02 +0300

    init

---
+++ Tasker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Tasker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tasker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tasker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
